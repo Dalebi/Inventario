f35be991ad244362b8707929052ee9f88285ef6b
Dalebi <calebi.cbi@gmail.com>
2023-03-30 13:05:27 -0600

    Routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::controller(ProductController::class)->group(function () {
+    Route::get('/orders', 'index');
+    Route::get('/orders/{id}', 'show');
+    Route::post('/orders', 'store');
+    Route::put('/orders/{id}', 'update');
+    Route::get('/orders/{id}/delete', 'delete');
+    Route::get('/orders/{id}/edit', 'edit');
+    Route::get('/orders/create', 'create');
+
+
+});
+
+
+//Route::get('/products', [ProductController::class, 'index'])->name('products.index');
